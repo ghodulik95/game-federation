25acecc8f4eaf079f41cf2fab75b932c085bc045
George Hodulik <ghodulik95@gmail.com>
2024-12-02 14:20:48 -0500

    Fleshing out commented todos for MastodonFederationService

--- MastodonFederationService.js
+++ MastodonFederationService.js
@@ -7,26 +7,37 @@ export default class MastodonFederationService extends FederationService {
     
     onNewFollow(username, serverInstance) {
         // If server instance is this one (social.collectivemoo.net)
-        // Add user @username
-        // Else add user @username@serverInstance
+        // username = @username
+        // Else
+        // username = @username@serverInstance
+        // 
+        // Check if username exists in the users table of the mysql database (column name is username)
+        // If not, make a new user with command:
+        // INSERT INTO users (username, password, coins) VALUES ('username', 'bcrypt-hashed-password', 999999);
+        // Replacing username with the username, and bcrypt-hased-password with a randomly generated,
+        // easily human readable password, that is hashed with bcrypt 10 times.
+        // Finally, send a private toot to @username@serverInstance sending them their randomly generated password.
+        // and telling them the URL for the yukon server.
+    }
+    
+    start() {
+        super.start()
         
-        // For now probably just set password to testpass
-        // In the future, maybe send dm to new follower with password
-        
-        // (Maybe make a simple webpage for resetting password)
+        // Initialize local mysql database connection using user yukon and passwork my_password
+        // Use database yukon
     }
     
     subscribeToFederatedStream() {
-        // TODO: Listen for toots from Mastodon client, and
-        // forward to this.onFederatedMessageReceived(message)
+        // Using mastodon api, connect to user @yukon-dev@social.collectivemoo.net
         
-        // Listen for all toots from followers, as well as self (?)
+        // Set up listening so that on a new toot from someone yukon-dev follows, or a toot from themselves,
+        // the toot content is passed as json and forwarded to this.onFederatedMessageReceived(message)
+        
         return true
     }
 
     federateMessage(message) {
-        // TODO: Send out to Mastodon as toot
-        // Followers only
-        this.onFederatedMessageReceived(message)
+        // Send the message as json content within a mastodon toot from the
+        // @yukon-dev@social.collectivemoo.net account, privacy set to followers only.
     }
 }
